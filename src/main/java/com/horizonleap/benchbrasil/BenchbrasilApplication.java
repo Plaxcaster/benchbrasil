f5f6afe4a930e5d373f238a533063a3c08404b4c
Pedro Plá <pedro.bpla@gmail.com>
2024-05-26 17:40:44 -0300

    Projeto base spring
    Adicionado projeto base spring

---
+++ src/main/java/com/horizonleap/benchbrasil/BenchbrasilApplication.java
@@ -0,0 +1,13 @@
+package com.horizonleap.benchbrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenchbrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenchbrasilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/horizonleap/benchbrasil/BenchbrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horizonleap.benchbrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchbrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
